a46fce7dc6dccb2a31861b9fe09e6abbdff4a034
SoapyFridge <tara.adamczyk@hotmail.com>
2026-01-02 11:08:25 +0100

    Aanpassing foto's

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Profile;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileSeeder extends Seeder
 {
@@ -18,13 +19,21 @@ class ProfileSeeder extends Seeder
             return;
         }
 
-        $profilePictures = collect(
-            File::files(storage_path('app/public/profiles'))
-        )->map(fn ($file) => 'profiles/' . $file->getFilename());
+        $profileDir = storage_path('app/public/profiles');
+        $motoDir = storage_path('app/public/moto');
 
-        $motoPictures = collect(
-            File::files(storage_path('app/public/moto'))
-        )->map(fn ($file) => 'moto/' . $file->getFilename());
+        $profilePictures = collect();
+        $motoPictures = collect();
+
+        if (is_dir($profileDir)) {
+            $profilePictures = collect(File::files($profileDir))
+                ->map(fn ($file) => 'profiles/' . $file->getFilename());
+        }
+
+        if (is_dir($motoDir)) {
+            $motoPictures = collect(File::files($motoDir))
+                ->map(fn ($file) => 'moto/' . $file->getFilename());
+        }
 
         foreach ($users as $index => $user) {
             Profile::query()->updateOrCreate(
@@ -32,8 +41,12 @@ class ProfileSeeder extends Seeder
                 [
                     'birthday' => now()->subYears(rand(20, 55))->subDays(rand(0, 365)),
                     'bio' => 'Motorrijder uit Gent met passie voor rijden en community.',
-                    'profile_picture' => $profilePictures->random(),
-                    'moto_picture' => $motoPictures->random(),
+                    'profile_picture' => $profilePictures->isNotEmpty()
+                        ? $profilePictures->random()
+                        : null,
+                    'moto_picture' => $motoPictures->isNotEmpty()
+                        ? $motoPictures->random()
+                        : null,
                 ]
             );
         }
